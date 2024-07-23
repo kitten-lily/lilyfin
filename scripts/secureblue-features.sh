3b4fc56c962b1351120d317602db8b4babacf55e
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-07-23 14:24:51 +0200

    Update secureblue-features.sh

--- scripts/secureblue-features.sh
+++ scripts/secureblue-features.sh
@@ -6,14 +6,14 @@ set -ouex pipefail
 wget https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/etc/NetworkManager/conf.d/rand_mac.conf -O /usr/etc/NetworkManager/conf.d/rand_mac.conf
 
 # Lock down firewall
-wget https://raw.githubusercontent.com/secureblue/secureblue/live/config/files/usr/etc/firewalld/zones/FedoraWorkstation.xml -O /usr/etc/firewalld/zones/FedoraWorkstation.xml
+wget https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/etc/firewalld/zones/FedoraWorkstation.xml -O /usr/etc/firewalld/zones/FedoraWorkstation.xml
 
 # Configure chronyd to use Network Time Security (NTS)
-wget https://raw.githubusercontent.com/secureblue/secureblue/live/config/files/usr/etc/chrony.conf -O /usr/etc/chrony.conf
-wget https://raw.githubusercontent.com/secureblue/secureblue/live/config/files/usr/etc/sysconfig/chronyd -O /usr/etc/sysconfig/chronyd
+wget https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/etc/chrony.conf -O /usr/etc/chrony.conf
+wget https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/etc/sysconfig/chronyd -O /usr/etc/sysconfig/chronyd
 
 # sudo timeout to 1min instead of 0min
-wget https://raw.githubusercontent.com/secureblue/secureblue/live/config/files/usr/etc/sudoers.d/timeout -O /usr/etc/sudoers.d/timeout
+wget https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/etc/sudoers.d/timeout -O /usr/etc/sudoers.d/timeout
 
 # Disable passwordless sudo for rpm-ostree install
-wget https://raw.githubusercontent.com/secureblue/secureblue/live/config/files/usr/share/polkit-1/rules.d/org.projectatomic.rpmostree1.rules -O /usr/share/polkit-1/rules.d/org.projectatomic.rpmostree1.rules
+wget https://raw.githubusercontent.com/secureblue/secureblue/live/files/system/usr/share/polkit-1/rules.d/org.projectatomic.rpmostree1.rules -O /usr/share/polkit-1/rules.d/org.projectatomic.rpmostree1.rules
