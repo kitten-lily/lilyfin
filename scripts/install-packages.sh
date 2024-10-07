2dab7137f1a5c4bb9a4257703f016de7c4f24876
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-10-07 17:42:12 +0200

    feat: Add upstream rclone

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 pass_url=$(wget -q -O- https://proton.me/download/PassDesktop/linux/x64/version.json | jq -r '.Releases | max_by(.ReleaseDate) | .File[] | select(.Identifier==".rpm (Fedora/RHEL)").Url')
 
+# Install latest rclone to get Proton support
+rpm-ostree override replace https://downloads.rclone.org/rclone-current-linux-amd64.rpm
+
 # Download any packages that we can't get from any repos.
 wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.rpm -O /tmp/ProtonMail-desktop-beta.rpm
 wget $pass_url -O /tmp/ProtonPass.rpm
