56f85688c2304bed13aca251120141b94e348f00
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-11-21 19:30:32 +0100

    Disable warp and brave

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ RELEASE="$(rpm -E %fedora)"
 
 . /tmp/repos.sh
 . /tmp/1password.sh
-. /tmp/brave.sh
-. /tmp/warp.sh
+#. /tmp/brave.sh
+#. /tmp/warp.sh
 . /tmp/install-packages.sh
 . /tmp/secureblue-features.sh
 . /tmp/cleanup.sh
